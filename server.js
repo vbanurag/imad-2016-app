2c1671cdc172d296c37168996cc698e6a8f4bc02
anurag sharma <vbanurag@users.noreply.github.com>
2016-09-19 19:41:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,36 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+        title: 'bio | Anurag Sharma | Entrepreneur',
+        heading: 'timeline',
+        date: '12-Sep-2016',
+        content: `
+        <h3>Academics<h3>
+        <ol>
+            <li><h3>Graduation<h3> : <p> Keshav mahavidalaya</p>
+            <li><h3>Post Graduation<h3> : <p> JIMS </p>
+            <li><h3>Post Doc<h3> : <p> IIT Delhi </p>
+        </ol>
+        `
+    
+};
+
+function createTemplate(data)
+{
+    var htmlTemplate = `
+        <html>
+        <head>
+            <title>
+        
+            </title>
+        </head>
+        <meta name = "viewport" content = "width=device-width, intial-scale=1"/>
+        <link href = "/ui/style.css" rel = "stylesheet"/>
+        </html>
+    `;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
