999a6dd934745a47f1bbb570c7ac77f79e666af0
anurag sharma <vbanurag@users.noreply.github.com>
2016-09-24 00:05:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,14 +32,33 @@ var nam = inputName.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to send sever and snd a name
+    // create a request to counter end point
+    var request = new  XMLHttpRequest();
+    
+    //Captue the response and store into the variable
+    
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE) {
+           //take some action
+           if(request.status === 200){
+               
+               var names = request.responseText;
+               names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i<names.length; i++)
+                {
+                    list += '<li>' + names[i] +'</li>';
+                }
+                var ul = document.getElementById('nameList');
+                ul.innerHTML = list;
+            }
+       }
+       //do nothing
+    };
+    //Make a Request
+    request.open('GET','http://vbanurag.imad.hasura-app.io/submit-names?name=' + nam,true);
+    request.send(null);
     //capture the list of list and rebder as list
     
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for (var i = 0; i<names.length; i++)
-    {
-        list += '<li>' + names[i] +'</li>';
-    }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
+    
 };
