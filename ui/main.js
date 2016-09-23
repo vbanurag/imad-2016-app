31b304fa185d97a653c0f070100c09a3946020da
anurag sharma <vbanurag@users.noreply.github.com>
2016-09-23 22:31:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,18 @@
-console.log('Loaded!');
+//counter
 
-//dynamic code change
-var element = document.getElementById('text-area');
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+    
+    // Make a request to counter end point
+    
+    
+    //Captue the response and store into the variable
+    
+    //Render the variable in counter sapn
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
 
-element.innerHTML = 'Hello world'
